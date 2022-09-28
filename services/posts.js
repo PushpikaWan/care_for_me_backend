ef78882891a8a05b8b4114db1f10922e549bcbf6
Pushpika Wanniachchi <pushpika@grubtech.com>
2022-09-29 00:33:54 +0530

    updated post added

--- services/posts.js
+++ services/posts.js
@@ -90,6 +90,127 @@ module.exports.deletePost = async (options) => {
 
 /**
  * @param {Object} options
+ * @param {String} options.postId post id
+ * @throws {Error}
+ * @return {Promise}
+ */
+module.exports.addComment = async (options) => {
+  // Implement your business logic here...
+  //
+  // This function should return as follows:
+  //
+  // return {
+  //   status: 200, // Or another success code.
+  //   data: [] // Optional. You can put whatever you want here.
+  // };
+  //
+  // If an error happens during your business logic implementation,
+  // you should throw an error as follows:
+  //
+  // throw new ServerError({
+  //   status: 500, // Or another error code.
+  //   error: 'Server Error' // Or another error message.
+  // });
+
+  return {
+    status: 200,
+    data: 'addComment ok!'
+  };
+};
+
+/**
+ * @param {Object} options
+ * @param {String} options.postId post id
+ * @throws {Error}
+ * @return {Promise}
+ */
+module.exports.deleteComment = async (options) => {
+  // Implement your business logic here...
+  //
+  // This function should return as follows:
+  //
+  // return {
+  //   status: 200, // Or another success code.
+  //   data: [] // Optional. You can put whatever you want here.
+  // };
+  //
+  // If an error happens during your business logic implementation,
+  // you should throw an error as follows:
+  //
+  // throw new ServerError({
+  //   status: 500, // Or another error code.
+  //   error: 'Server Error' // Or another error message.
+  // });
+
+  return {
+    status: 200,
+    data: 'deleteComment ok!'
+  };
+};
+
+/**
+ * @param {Object} options
+ * @param {String} options.postId post id
+ * @throws {Error}
+ * @return {Promise}
+ */
+module.exports.reportPost = async (options) => {
+  // Implement your business logic here...
+  //
+  // This function should return as follows:
+  //
+  // return {
+  //   status: 200, // Or another success code.
+  //   data: [] // Optional. You can put whatever you want here.
+  // };
+  //
+  // If an error happens during your business logic implementation,
+  // you should throw an error as follows:
+  //
+  // throw new ServerError({
+  //   status: 500, // Or another error code.
+  //   error: 'Server Error' // Or another error message.
+  // });
+
+  return {
+    status: 200,
+    data: 'reportPost ok!'
+  };
+};
+
+/**
+ * @param {Object} options
+ * @param {String} options.postId post id
+ * @param {String} options.commentId comment id
+ * @throws {Error}
+ * @return {Promise}
+ */
+module.exports.reportComment = async (options) => {
+  // Implement your business logic here...
+  //
+  // This function should return as follows:
+  //
+  // return {
+  //   status: 200, // Or another success code.
+  //   data: [] // Optional. You can put whatever you want here.
+  // };
+  //
+  // If an error happens during your business logic implementation,
+  // you should throw an error as follows:
+  //
+  // throw new ServerError({
+  //   status: 500, // Or another error code.
+  //   error: 'Server Error' // Or another error message.
+  // });
+
+  return {
+    status: 200,
+    data: 'reportComment ok!'
+  };
+};
+
+/**
+ * @param {Object} options
  * @param {String} options.userId user id
  * @throws {Error}
  * @return {Promise}
