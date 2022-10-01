fed9c1a62321dd85f3ee9dc512a6175685e7fe63
Pushpika Wanniachchi <pushpika@grubtech.com>
2022-10-02 02:33:39 +0530

    db connection field added

--- db/db-config.js
+++ db/db-config.js
@@ -2,7 +2,6 @@ const {STATE_ACTIVE} = require("../util/constants");
 const MongoClient = require("mongodb").MongoClient;
 
 const connectionString = process.env.MONGODB_URI;
-// const connectionString = 'mongodb+srv://PushpikaWan:lucefer%401234@carefmcluster.k1puddk.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'CARE_FOR_ME_DEV';
 
 getDB = async () => {
