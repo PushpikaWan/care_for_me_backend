42a180d0d850bae4ceac648d30c62ae40cbd661d
Pushpika Wanniachchi <pushpika@grubtech.com>
2022-10-05 12:39:39 +0530

    [7] feat: private key issue resolving check

--- authorization/firebase-admin.js
+++ authorization/firebase-admin.js
@@ -1,24 +1,20 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = `{
+const serviceAccount = {
   "type": "service_account",
-  "project_id": ${process.env.PROJECT_ID},
-  "private_key_id": ${process.env.PRIVATE_KEY_ID},
-  "private_key": ${JSON.stringify(
-    process.env.PRIVATE_KEY.replace(/\\n/g, '\n'))},
-  "client_email": ${process.env.CLIENT_EMAIL_ADDRESS},
-  "client_id": ${process.env.CLIENT_ID},
-  "auth_uri": ${process.env.AUTH_URI},
-  "token_uri": ${process.env.TOKEN_URI},
-  "auth_provider_x509_cert_url": ${process.env.AUTH_PROVIDER_URI},
-  "client_x509_cert_url": ${process.env.CLIENT_CERTIFCATE_URL}
-}`;
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL_ADDRESS,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_URI,
+  "client_x509_cert_url": process.env.CLIENT_CERTIFCATE_URL
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.PROJECT_ID,
-    privateKey: JSON.stringify(process.env.PRIVATE_KEY),
-    clientEmail: process.env.CLIENT_EMAIL_ADDRESS
-  })
+  credential: admin.credential.cert(
+      JSON.parse(Buffer.from(serviceAccount, 'base64').toString('utf8')))
 });
 
